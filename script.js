a85febbcf05c1f0beff29f052af2f3d096d3880b
Ivan <kwakiovoono@gmail.com>
2022-12-11 21:41:54 +0100

    Add skeleton content

---
+++ script.js
@@ -0,0 +1,16 @@
+function validateForm(event) {
+    event.preventDefault()
+}
+
+let myLibrary = []
+
+function Book() {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+        
+}
